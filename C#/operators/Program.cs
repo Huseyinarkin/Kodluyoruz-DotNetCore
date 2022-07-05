96d3f112c33434b9634f964e3b85811822d0f530
Huseyinarkin <99.arkn@gmail.com>
2022-07-05 14:40:32 +0300

    Operators

---
+++ C#/operators/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Operators");
+// Atama ve İşlemli Atama Operatörleri (=, +=, -=, *=, /=)
+    int x=6;
+    Console.WriteLine(x.ToString()); // 6
+    x+=3; // x = x + 3;
+    Console.WriteLine(x.ToString()); // 9
+    x -= 4; // x = x - 4;
+    Console.WriteLine(x.ToString()); // 5
+    x *= 6; // x = x * 6;
+    Console.WriteLine(x.ToString()); // 25
+    x /= 10; // x = x / 10;
+    Console.WriteLine(x.ToString()); // 5
+
+// Mantıksal Operatörler (||, &&, !)
+    bool isSuccess = true;
+    bool isCompleted = false;
+    Console.WriteLine(isSuccess && isCompleted); // isSuccess ve isCompleted True mu ?
+    Console.WriteLine(isSuccess || isCompleted); // isSuccess veya isCompleted True mu ?
+    Console.WriteLine(!isSuccess); // Değilse
+
+// İlişkisel Operatörler (==,!=, <, >, >=,<=)
+
+// Aritmetik (+, -, *, /, %, ++, --)
